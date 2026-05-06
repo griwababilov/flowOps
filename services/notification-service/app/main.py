f8840d0ec02183e55b21da7d6302bf85ce12356c
griwababilov <egorrybakov2906@gmail.com>
2026-05-06 19:28:16 +0300

    notification router

--- services/notification-service/app/main.py
+++ services/notification-service/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from app.api.notification_router import router
 
 app = FastAPI(title="Notification Service")
 
+app.include_router(router)
 
 @app.get("/health")
 def health():
