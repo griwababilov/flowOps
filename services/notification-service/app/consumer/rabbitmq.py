eb39d1eae35431c3ea0c68fa30e155f1115f7e21
griwababilov <egorrybakov2906@gmail.com>
2026-05-16 12:45:03 +0300

    lil fix rabbitmq

--- services/notification-service/app/consumer/rabbitmq.py
+++ services/notification-service/app/consumer/rabbitmq.py
@@ -1,9 +1,17 @@
+import logging
+import os
+import time
+
 from pika import BlockingConnection, URLParameters
 from pika.adapters.blocking_connection import BlockingChannel
-import os
+from pika.exceptions import AMQPConnectionError
+
+
+log = logging.getLogger(__name__)
 
 EXCHANGE_NAME: str = "flowops.events"
 QUEUE_NAME: str = "notification-service.queue"
+
 ROUTING_KEYS: tuple[str, ...] = (
     "batch.completed",
     "part.defective_detected",
@@ -11,9 +19,22 @@ ROUTING_KEYS: tuple[str, ...] = (
 
 
 def get_connection() -> BlockingConnection:
-    connection = BlockingConnection(URLParameters(os.getenv("RABBITMQ_URL")))
+    rabbitmq_url = os.getenv("RABBITMQ_URL")
 
-    return connection
+    if rabbitmq_url is None:
+        raise RuntimeError("RABBITMQ_URL is not set")
+
+    parameters = URLParameters(rabbitmq_url)
+
+    while True:
+        try:
+            connection = BlockingConnection(parameters)
+            log.info("Connected to RabbitMQ")
+            return connection
+
+        except AMQPConnectionError:
+            log.warning("RabbitMQ is not ready yet. Retrying in 3 seconds...")
+            time.sleep(3)
 
 
 def get_channel() -> tuple[BlockingConnection, BlockingChannel]:
@@ -21,12 +42,21 @@ def get_channel() -> tuple[BlockingConnection, BlockingChannel]:
     channel = connection.channel()
 
     channel.exchange_declare(
-        exchange=EXCHANGE_NAME, exchange_type="topic", durable=True
+        exchange=EXCHANGE_NAME,
+        exchange_type="topic",
+        durable=True,
     )
-    channel.queue_declare(queue=QUEUE_NAME, durable=True)
+
+    channel.queue_declare(
+        queue=QUEUE_NAME,
+        durable=True,
+    )
+
     for routing_key in ROUTING_KEYS:
         channel.queue_bind(
-            exchange=EXCHANGE_NAME, queue=QUEUE_NAME, routing_key=routing_key
+            exchange=EXCHANGE_NAME,
+            queue=QUEUE_NAME,
+            routing_key=routing_key,
         )
 
     return connection, channel
